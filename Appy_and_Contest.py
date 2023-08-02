002e01251b2f16d69f9be3ac299bba7a94b9d341
Jinnala Harshita <119839384+Harshitajinnala@users.noreply.github.com>
2023-08-02 13:53:16 +0530

    n=int(input()) for i in range(n):     a,b,c,d=map(int,input().split())     x=(a/b)+(a/c)     if (x>=d):         print("Win")     else:         print("Lose")

--- Appy_and_Contest.py
+++ Appy_and_Contest.py
@@ -1,8 +1,8 @@
 n=int(input())
 for i in range(n):
     a,b,c,d=map(int,input().split())
-    x=a/b+a/c
-    if(x>=d):
+    x=(a/b)+(a/c)
+    if (x>=d):
         print("Win")
     else:
         print("Lose")
