87cfb69524cd1ec1c99f3cfb99bcf6d2e895c299
Jinnala Harshita <119839384+Harshitajinnala@users.noreply.github.com>
2023-08-02 14:36:44 +0530

    a=int(input()) b=int(input()) for i in range(a,b+1):     c=0     z=str(i)     for s in str(i):         if int(s)!=0:             if i % int(s)==0:                 c+=1     if len(z)==c:         print(i,end=' ')

---
+++ Self_Dividing_Numbers.py
@@ -0,0 +1,11 @@
+a=int(input())
+b=int(input())
+for i in range(a,b+1):
+    c=0
+    z=str(i)
+    for s in str(i):
+        if int(s)!=0:
+            if i % int(s)==0:
+                c+=1
+    if len(z)==c:
+        print(i,end=' ')
